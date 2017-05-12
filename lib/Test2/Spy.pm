5ee1c217ab434f10494058c709d06d345c8612ef
Joel Berger <joel.a.berger@gmail.com>
2017-05-12 10:19:28 +0200

    also overload the run_timeout method

--- lib/Test2/Spy.pm
+++ lib/Test2/Spy.pm
@@ -25,32 +25,59 @@ sub test {
   #}
 
   local $ENV{PERL5OPT} = $ENV{PERL5OPT} . " -MTest2::Spy::Bug=ws://127.0.0.1:$port/";
+  our $TIMEOUT;
+
+  no warnings 'redefine';
+
+  local *App::cpanminus::script::run_timeout = sub {
+    my($self, $cmd, $timeout) = @_;
+    local $TIMEOUT = $timeout if $timeout;
+    return $self->run($cmd);
+  };
 
   local *App::cpanminus::script::run = sub {
     my($self, $cmd) = @_;
     my $pid;
     if ($self->WIN32) {
-        $cmd = $self->shell_quote(@$cmd) if ref $cmd eq 'ARRAY';
-        unless ($self->{verbose}) {
-            $cmd .= " >> " . $self->shell_quote($self->{log}) . " 2>&1";
-        }
-        $pid = !system 1, $cmd;
+      $cmd = $self->shell_quote(@$cmd) if ref $cmd eq 'ARRAY';
+      unless ($self->{verbose}) {
+        $cmd .= " >> " . $self->shell_quote($self->{log}) . " 2>&1";
+      }
+      $pid = !system 1, $cmd;
     } else {
-        $pid = fork;
-        unless ($pid) {
-            $self->run_exec($cmd);
-        }
+      $pid = fork;
+      unless ($pid) {
+        $self->run_exec($cmd);
+      }
     }
-    Mojo::IOLoop->recurring(1 => sub {
+
+    my $rid = Mojo::IOLoop->recurring(1 => sub {
       Mojo::IOLoop->stop if waitpid($pid, WNOHANG) > 0;
     });
+
+    my $tid = $TIMEOUT ? Mojo::IOLoop->timer($TIMEOUT => sub {
+      Mojo::IOLoop->stop;
+      $self->diag_fail("Timed out (> ${TIMEOUT}s). Use --verbose to retry.");
+      local $SIG{TERM} = 'IGNORE';
+      kill TERM => 0;
+      waitpid $pid, 0;
+    }) : undef;
+
     local $SIG{HUP} = sub { Mojo::IOLoop->stop };
+
     Mojo::IOLoop->start;
+    $_ && Mojo::IOLoop->remove($_) for ($rid, $tid);
+
+    return 1;
   };
 
   $orig->(@_);
 
-  print STDERR $spy->format_result($_) for @{ $spy->results };
+  my $results = $spy->results;
+  print $spy->format_result($_) for @$results;
+  print @$results . " tests run\n";
+
+  return 1;
 }
 
 CHECK {
